659613ec4f472b3b7a070b9df10f65e1bcb70b9d
c4605 <bolasblack@gmail.com>
2026-05-18 13:45:05 +0200

    fix(ci): update react version switcher

--- scripts/change_react_deps_version.cjs
+++ scripts/change_react_deps_version.cjs
@@ -1,30 +1,26 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.writeFileSync(
-  path.resolve(__dirname, '../package.json'),
-  JSON.stringify(
-    updateVersion(process.argv[2], require('../package.json')),
-    null,
-    '  ',
-  ),
-)
+if (require.main === module) {
+  fs.writeFileSync(
+    path.resolve(__dirname, '../package.json'),
+    JSON.stringify(
+      updateVersion(process.argv[2], require('../package.json')),
+      null,
+      '  ',
+    ),
+  )
+}
 
 function updateVersion(version, pkgs) {
-  const resolutions = { ...pkgs.resolutions }
-
-  const reactResolutionKeys = Object.keys(pkgs.resolutions).filter(
-    dep =>
-      !dep.includes('@types') &&
-      (/\/react$/.test(dep) || /\/react-dom$/.test(dep)),
-  )
-
-  reactResolutionKeys.forEach(key => {
-    resolutions[key] = version
-  })
-
   return {
     ...pkgs,
-    resolutions,
+    dependencies: {
+      ...pkgs.dependencies,
+      react: version,
+      'react-dom': version,
+    },
   }
 }
+
+module.exports = { updateVersion }
